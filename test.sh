dbb722f3a966ed74ee4159c3dd52a3dbc666da23
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-07-03 01:53:09 +0200

    Add test case

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TESTDIR="${DIR}/test"
+
+BUILD=$(docker build -q ${DIR})
+
+echo $BUILD
+
+rm -f "${TESTDIR}/test.pdf"
+
+docker run -v "${TESTDIR}":/target "$BUILD" --enable-write18 ./test.tex \
+	&& open "${TESTDIR}/test.pdf"

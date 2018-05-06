06d13340709c4f8c31120eff818ad9ca0197695d
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-05-06 18:09:29 +0200

    Better testing env

--- test.sh
+++ test.sh
@@ -7,8 +7,10 @@ BUILD=$(docker build -q -t jelmervdl/latexmk ${DIR})
 
 echo $BUILD
 
-rm -f "${TESTDIR}/tmp/*.{pdf,hasl}"
+rm -f "${TESTDIR}/tmp/*.{pdf,hasl,log,tex}"
 rm -f "${TESTDIR}/test.pdf"
 
 docker run -v "${TESTDIR}":/target "$BUILD" ./test.tex \
 	&& open "${TESTDIR}/test.pdf"
+
+cat "${TESTDIR}/test.log"

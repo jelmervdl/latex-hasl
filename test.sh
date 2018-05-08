95a77d71a1ca8cd8a9b18d5be8ac1d6b37548c43
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-05-08 11:30:46 +0200

    Fix tests

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@ BUILD=$(docker build -q -t jelmervdl/latexmk ${DIR})
 
 echo $BUILD
 
-rm -f "${TESTDIR}/tmp/*.{pdf,hasl,log,tex}"
-rm -f "${TESTDIR}/test.pdf"
+rm -f ${TESTDIR}/tmp/*.{pdf,hasl,log,tex}
+rm -f ${TESTDIR}/test.pdf
 
 docker run -v "${TESTDIR}":/target "$BUILD" ./test.tex \
 	&& open "${TESTDIR}/test.pdf"
